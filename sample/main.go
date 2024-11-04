c2bb7f85c92c27c7a8fcf0c6d077934146fd309b
bodrovis <bodrovis@protonmail.com>
2024-11-04 21:29:13 +0200

    bump

--- sample/main.go
+++ sample/main.go
@@ -10,14 +10,15 @@ func main() {
 	k2 := os.Getenv("K2")
 	k3 := os.Getenv("K3")
 
+	// Log other information to stderr
 	if k1 == "" || k2 == "" {
-		fmt.Println("Missing required environment variables.")
+		fmt.Fprintln(os.Stderr, "Missing required environment variables.")
 		os.Exit(1)
 	}
 
-	fmt.Printf("API Token: %s\n", k1)
-	fmt.Printf("Project ID: %s\n", k2)
-	fmt.Printf("Base Language: %s\n", k3)
+	fmt.Fprintf(os.Stderr, "API Token: %s\n", k1)
+	fmt.Fprintf(os.Stderr, "Project ID: %s\n", k2)
+	fmt.Fprintf(os.Stderr, "Base Language: %s\n", k3)
 
 	output := "some output value"
 	fmt.Println(output)
